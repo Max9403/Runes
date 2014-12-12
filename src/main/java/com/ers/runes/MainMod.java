bdd6f34e9265f360afdfa1e09591f5e016380cb7
Benjamin Claassen <BClaassen@live.com>
2014-12-12 12:46:49 +0200

    Dependencies

---
+++ src/main/java/com/ers/runes/MainMod.java
@@ -0,0 +1,13 @@
+package com.ers.runes;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * Created by Benjamin on 2014-12-12.
+ */
+@Mod(modid = MainMod.MODID, version = MainMod.VERSION, name = MainMod.NAME)
+public class MainMod {
+    public static final String MODID = "runes";
+    public static final String NAME = "Runes";
+    public static final String VERSION = "1.0.0";
+}
